9b5873a300b26b6152f5518ef3af4c13b4721391
pprad <pprad@DESKTOP-AHV403J>
2022-10-15 21:42:56 +0530

    new event module committed

---
+++ Maven_zohoCRM/src/test/java/com/zohoCRM/testscripts/NewEventModule.java
@@ -0,0 +1,16 @@
+package com.zohoCRM.testscripts;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import com.zohoCRM.generic.BaseClass;
+
+@Listeners(com.zohoCRM.generic.ListenerImplementation.class)
+
+public class NewEventModule extends BaseClass {
+	@Test
+	public void createNewEvent() {
+		
+	}
+
+}
